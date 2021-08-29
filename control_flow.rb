3f16d5b6ea6d54af03f5c30d857b3d8cfb1af072
Coy Reid <coyreid7@gmail.com>
2021-08-29 14:40:17 -0500

    commit tree dec61d5bfc33462289cd6d817e253df833cefc50

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,52 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  output = if temperature < 40
+              "brisk"
+           elsif temperature >= 40 && temperature <= 65
+              "a little chilly"
+           elsif temperature > 85
+              "too dang hot"
+           else
+              "perfect"
+           end
+  "It's #{output} out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else 
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else puts "Invalid operation!"
+  end
 end
 
+
+#alternative to the caluclator method. The .send lets you uses a string or symbol as a method and converts it. 
+def alt_calculator(operation, num1, num2)
+  if ["+", "-", "*", "/"].include?(operation)
+    num1.send(operation, num2)
+  else
+    puts "Invalid operation!"
+  end
+end
